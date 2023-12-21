b32e91d17058c763dd13d89de4716a38f278d68f
Adarsh Kamath <adarshkmt2004@gmail.com>
2023-12-21 16:42:18 +0530

    added @NotNull validation for comment body.

--- src/main/java/kamathadarsh/Conduit/Request/CommentRequest.java
+++ src/main/java/kamathadarsh/Conduit/Request/CommentRequest.java
@@ -1,5 +1,7 @@
 package kamathadarsh.Conduit.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,7 @@ import lombok.NoArgsConstructor;
 
 public class CommentRequest {
 
+    @NotNull(message = "comment body must not be null.")
+    @NotBlank(message = "comment body must not be blank.")
     private String body;
 }
