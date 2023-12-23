170d68cc711ab1da0a959bf80c47cbe23accf676
Adarsh Kamath <adarshkmt2004@gmail.com>
2023-12-23 21:54:35 +0530

    new method to build congratulatory email template.

--- src/main/java/kamathadarsh/Conduit/Service/EmailContentBuilderService.java
+++ src/main/java/kamathadarsh/Conduit/Service/EmailContentBuilderService.java
@@ -16,16 +16,29 @@ public class EmailContentBuilderService {
 
     private static final String link1 = "http://localhost:8080/api/user/";
     private static final String link2 = "/articles/";
-    String buildEmailTemplate(String emailTitle, String recipientUsername, List<EmailArticleDTO> dailyArticles){
+    String buildDailyDigestEmailTemplate(String recipientUsername, List<EmailArticleDTO> dailyArticles){
 
         Context thymeLeafContext = new Context();
 
-        thymeLeafContext.setVariable("dailyTitle", emailTitle);
+        thymeLeafContext.setVariable("dailyTitle", "Conduit Daily Digest");
         thymeLeafContext.setVariable("articles", dailyArticles);
         thymeLeafContext.setVariable("link", link1 + recipientUsername + link2);
 
-        String emailContent = templateEngine.process("DailyArticleDigestEmailTemplate", thymeLeafContext);
+        return templateEngine.process("DailyArticleDigestEmailTemplate", thymeLeafContext);
 
-        return emailContent;
+
+    }
+
+    String buildCongratulatoryEmailTemplate(String recipientUsername, String articleTitle, Integer numberOfLikes){
+
+        Context thymeLeafContext = new Context();
+
+        String congratulatoryMessage = "congratulations "+ recipientUsername+", your article \"" + articleTitle + "\" has " + numberOfLikes + " likes.";
+        thymeLeafContext.setVariable("title", "Your Article is Popular");
+        thymeLeafContext.setVariable("body", congratulatoryMessage);
+
+        return templateEngine.process("CongratulatoryEmailTemplate", thymeLeafContext);
+
+
     }
 }
