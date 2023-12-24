a185cf3c1becb25c2535d5a094d0984fd6f74921
Adarsh Kamath <adarshkmt2004@gmail.com>
2023-12-24 18:19:47 +0530

    changed sendCongratulatoryEmail method to take CongratulatoryEmailView pojo object as argument. this pojo object contains relevant info required for sending email to author.

--- src/main/java/kamathadarsh/Conduit/Service/EmailService.java
+++ src/main/java/kamathadarsh/Conduit/Service/EmailService.java
@@ -1,12 +1,13 @@
 package kamathadarsh.Conduit.Service;
 
-import kamathadarsh.Conduit.DTO.EmailDTO.EmailUserDTO;
 import kamathadarsh.Conduit.Exception.MailNotSentException;
+import kamathadarsh.Conduit.jooq.jooqGenerated.tables.pojos.CongratulatoryEmailView;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,15 +17,19 @@ public class EmailService {
     private final JavaMailSender mailSender;
     private final EmailContentBuilderService emailContentBuilderService;
 
-    public void sendCongratulatoryEmail(EmailUserDTO recipientEmailUserDTO, String articleTitle, Integer numberOfLikes){
+    @Async
+    public void sendCongratulatoryEmail(CongratulatoryEmailView congratulatoryEmailInfo){
 
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom("conduit@email.com");
-            messageHelper.setTo(recipientEmailUserDTO.getEmailId());
+            messageHelper.setTo(congratulatoryEmailInfo.getEmailId());
             messageHelper.setSubject("Congratulations! Your Article is Popular!");
-            messageHelper.setText(emailContentBuilderService
-                    .buildCongratulatoryEmailTemplate(recipientEmailUserDTO.getUsername(), articleTitle, numberOfLikes));
+
+            messageHelper.setText(emailContentBuilderService.buildCongratulatoryEmailTemplate
+                    (congratulatoryEmailInfo.getUsername(),
+                            congratulatoryEmailInfo.getTitle(),
+                            congratulatoryEmailInfo.getNextMilestone()));
         };
         try{
             mailSender.send(messagePreparator);
